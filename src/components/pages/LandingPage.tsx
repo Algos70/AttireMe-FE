4ba95aa8bcc09141c4949084bf4a7f9a8d55be6a
Kürşat <draknes.gamin70@gmail.com>
2025-05-06 20:02:54 +0300

    feat: Landing page title change

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -7,6 +7,24 @@ import HeroSection from "../sections/HeroSection"
 import TrendingStyles from "../sections/TrendingStyles"
 
 const LandingPage = () => {
+    // change the title of the page to AttireMe
+    document.title = "AttireMe"
+    // change the meta description of the page to AttireMe
+    document
+        .querySelector("meta[name='description']")
+        ?.setAttribute("content", "AttireMe is a platform for creators and fans to connect and create together.")
+    // change the meta keywords of the page to AttireMe
+    document
+        .querySelector("meta[name='keywords']")
+        ?.setAttribute("content", "AttireMe, creators, fans, connect, create, together")
+    // change the meta author of the page to AttireMe
+    document
+        .querySelector("meta[name='author']")
+        ?.setAttribute("content", "AttireMe")
+    // change the meta og:title of the page to AttireMe
+    document
+        .querySelector("meta[property='og:title']")
+        ?.setAttribute("content", "AttireMe")
     return(
     <Layout>
         <HeroSection />
