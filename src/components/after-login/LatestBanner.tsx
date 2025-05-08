d30a25a74bd44232385d0739967a88bb20803f6e
Kürşat <draknes.gamin70@gmail.com>
2025-05-08 15:01:45 +0300

    feat: add latest banner a new png

--- src/components/after-login/LatestBanner.tsx
+++ src/components/after-login/LatestBanner.tsx
@@ -1,17 +1,18 @@
 import { FC } from 'react'
+import latestBanner from '../../assets/images/latest.png'
 
 // LatestBanner.tsx
 export const LatestBanner: FC = () => {
-    const imageUrl = 'https://picsum.photos/800/300?random=1';
     return (
       <div className="bg-page text-center py-8 px-4">
-        <img
-          src={imageUrl}
-          alt="Latest cover"
-          className="mx-auto rounded-lg shadow-lg w-32 sm:w-48 lg:w-64"
-        />
+        <div className="max-w-[384px] mx-auto">
+          <img
+            src={latestBanner}
+            alt="Latest cover"
+            className="w-full h-auto rounded-lg object-contain"
+          />
+        </div>
         <h2 className="mt-2 text-2xl sm:text-3xl font-semibold text-gray-800">Latest</h2>
       </div>
     );
   };
-  
